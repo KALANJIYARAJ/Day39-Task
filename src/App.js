e254066377152b32a86339e3d493202cb8c22ef0
kalanjiyaraj01@gmail.com <kalanjiyaraj01@gmail.com>
2022-11-13 11:52:00 +0530

    Folder Created Task complited

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import axios from "axios";
+import { useState } from "react";
 
 function App() {
+  const [data, setData] = useState("");
+  const createFolder = async () => {
+    try {
+      const folder = await axios.get("http://localhost:3003/filecreate");
+      setData(folder.data.message);
+    } catch (error) {
+      console.log("something Went wrong");
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container-lg">
+      <div className="row m-5">
+        <div className="col-6">
+          <button onClick={createFolder} className="btn btn-primary btn-sm">
+            Create Folder
+          </button>
+        </div>
+        <div className="col-6">
+          <h5>{data}</h5>
+        </div>
+      </div>
     </div>
   );
 }
